20b810b254b2147c16f072d63ed279c967606605
Jens Maus <mail@jens-maus.de>
2024-02-28 16:47:06 +0100

    fix prototype-polluting assignment by checking for __proto__

--- ical.js
+++ ical.js
@@ -548,7 +548,9 @@ module.exports = {
 
           // One more specific fix - in the case that an RRULE entry shows up after a RECURRENCE-ID entry,
           // let's make sure to clear the recurrenceid off the parent field.
-          if (typeof par[curr.uid].rrule !== 'undefined' && typeof par[curr.uid].recurrenceid !== 'undefined') {
+          if (typeof par[curr.uid].rrule !== 'undefined' &&
+              typeof par[curr.uid].recurrenceid !== 'undefined' &&
+              par[curr.uid] !== '__proto__') {
             delete par[curr.uid].recurrenceid;
           }
         } else if (component === 'VALARM' && (par.type === 'VEVENT' || par.type === 'VTODO')) {
